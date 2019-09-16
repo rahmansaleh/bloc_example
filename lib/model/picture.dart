9f59559a4841161573fa2915123055922e4f3b38
Muhammad Rahman Saleh <36500308+rahmansaleh@users.noreply.github.com>
2019-09-16 17:27:12 +0700

    Create picture.dart

---
+++ lib/model/picture.dart
@@ -0,0 +1,13 @@
+class Picture {
+  final String large;
+  final String medium;
+  final String thumbnail;
+
+  Picture(this.large, this.medium, this.thumbnail);
+
+  Picture.fromJson(Map<String, dynamic> json): 
+  large = json["large"],
+  medium = json["medium"],
+  thumbnail = json["thumbnail"]
+  ;
+}
