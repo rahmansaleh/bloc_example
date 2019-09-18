27a7fc91a0a14cee5d8d4448ca2c8ead7d95835c
Muhammad Rahman Saleh <36500308+rahmansaleh@users.noreply.github.com>
2019-09-18 16:36:23 +0700

    Update main.dart
    switch home route

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bloc_example/bloc/counter_bloc.dart';
+import 'package:bloc_example/user_widget.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: UserWidget(),
     );
   }
 }
