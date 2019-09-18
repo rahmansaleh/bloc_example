738ae6416d67cb0ab309a15e29c77764f4069010
Muhammad Rahman Saleh <36500308+rahmansaleh@users.noreply.github.com>
2019-09-18 16:35:50 +0700

    Update user_api_provider.dart
    adding _handleError

--- lib/provider/user_api_provider.dart
+++ lib/provider/user_api_provider.dart
@@ -1,19 +1,52 @@
-
+import 'package:bloc_example/model/info.dart';
 import 'package:bloc_example/model/user_response.dart';
 import 'package:dio/dio.dart';
 
 class UserApiProvider{
-  final String _endpoint = "https://randomuser.me/api/";
   final Dio _dio = Dio();
 
   Future<UserResponse> getUser() async {
     try {
-      Response response = await _dio.get(_endpoint);
+      Response response = await _dio.get("https://randomuser.me/api/");
       return UserResponse.fromJson(response.data);
     } catch (err,  stacktrace)  {
       print("Exception occured: $err stackTrace: $stacktrace");
-      return UserResponse.withError("$err");
+      return UserResponse.withError(_handleError(err));
     }
   }
 
+  String _handleError(err) {
+    String errorDescription = "";
+    if (Error is DioError) {
+      
+      switch (err.type) {
+        case DioErrorType.CANCEL:
+        errorDescription = "Request to API server was cancelled";
+        break;
+
+        case DioErrorType.CONNECT_TIMEOUT:
+        errorDescription = "Connection timeout with API server";
+        break;
+
+        case DioErrorType.DEFAULT:
+        errorDescription = "Connection to API server failed due to internet connection";
+        break;
+
+        case DioErrorType.RECEIVE_TIMEOUT:
+        errorDescription = "Receive timeout in connection with API server";
+        break;
+
+        case DioErrorType.RESPONSE:
+        errorDescription = "Received invalid status code: ${err.response.statusCode}";
+        break;
+        default:
+      }
+    } else {
+      errorDescription = "Unexpected error occured";
+    }
+
+    return errorDescription;
+  }
+
 }
+
